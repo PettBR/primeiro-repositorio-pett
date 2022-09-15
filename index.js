a208574e22c54102b246368942dda7afd1e7dd53
PettBR <felipe.pett@gmail.com>
2022-09-15 18:44:46 -0300

    Aula 2 com base do JavaScript

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * -> Tipos primitivos
+ * -> Variáveis do Javascript
+ * -> Operadores Matemáticos do Javascript
+ */
+
+//serve para escrever mensagens no console
+
+// string → textos
+// number → números
+// boolean → booleano (true or false)
+
+console.log("Hello world")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+// variáveis
+let mensagem = "Eu gosto de batata" // declaração de variável
+
+console.log (mensagem) //printa o valor da variável (no caso é uma string)
+
+mensagem = "Mensagem nova" // atribui novo valor a variável mensagem
+
+console.log (mensagem) //printa o novo valor da variável
