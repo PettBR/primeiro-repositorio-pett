c1a2807a74f2a46005ce52b611d281ef17c4fe01
PettBR <felipe.pett@gmail.com>
2022-09-22 18:44:10 -0300

    update - 22/09/2022 - estudo com laço for e while

--- desafio.js
+++ desafio.js
@@ -17,18 +17,43 @@ Código Condição de pagamento
  * No cartão
  */
 
+/** 
+ * As estruturas de Repetição, são estruturas que nos permitem repetir um código de uma maneira mais simples
+ * */
+// while → enquanto
+
 // recebendo o preço do produto e transformando o valor para decimal
-const precoProduto = parseFloat(prompt('Qual o preço do produto?'))
+let precoProduto = parseFloat(prompt('Qual o preço do produto?'));
+while(isNaN(precoProduto) || precoProduto <= 0) {
+    alert("O preço digitado é inválido, Por favor informe o preço apenas com números; sem caracteres especiais");
+    precoProduto = parseFloat(prompt('Qual o preço do produto?'));
+};
+
 const mensagem = `
 Informe o método de pagamento:
 Digite 1 para pagamento em dinheiro
 Digite 2 para pagamento em cheque
 Digite 3 para pagamento com cartão
-`
-const metodoDePagamento = prompt(mensagem)
+`;
+let metodoDePagamento = parseInt(prompt(mensagem));
 
-console.log(precoProduto)
-console.log(metodoDePagamento)
+
+
+/*
+Porque isso não funciona?
+while(metodoDePagamento != 1 || metodoDePagamento != 2 || metodoDePagamento != 3){
+    alert("Você digitou uma opção inválida. Por favor digite 1, 2 ou 3 de acordo com o método escolhido.");
+    metodoDePagamento = parseInt(prompt(mensagem));
+    console.log(metodoDePagamento);
+}
+*/
+
+while(isNaN(metodoDePagamento) == true || metodoDePagamento > 3 || metodoDePagamento <= 0){
+    alert("Você digitou uma opção inválida. Por favor digite 1, 2 ou 3 de acordo com o método escolhido.");
+    metodoDePagamento = parseInt(prompt(mensagem));
+}
+
+
 /**Se metodoDePagamento = 1 ou 2 */
 if (metodoDePagamento == 1 || metodoDePagamento == 2) {
 const precofinal = (precoProduto*0.9)
@@ -37,8 +62,13 @@ const precofinal = (precoProduto*0.9)
 alert(`O preço final do pagamento é ${precofinal.toFixed(2)} reais.`) 
 }
 else if (metodoDePagamento == 3){
-const parcelas = parseInt(prompt(`Pagamento em cartão escolhido:
+let parcelas = parseInt(prompt(`Pagamento em cartão escolhido:
 Informe a quantidade de parcelas.`))
+while(isNaN(parcelas) || parcelas < 1){
+    alert("Você digitou uma opção inválida. Por favor digite o número de parcelas apenas com números; sem caracteres especiais.");
+    parcelas = parseInt(prompt(`Pagamento em cartão escolhido:
+Informe a quantidade de parcelas.`));
+}
 if (parcelas == 1) {
     const precofinal = (precoProduto*0.85)
     alert(`O preço final do pagamento é ${precofinal.toFixed(2)} reais à vista.`) 
@@ -50,8 +80,4 @@ else if(parcelas >= 3){
     const precofinal = (precoProduto*1.1)
     alert(`O preço final do pagamento é ${precofinal.toFixed(2)} reais em ${parcelas} vezes de ${(precofinal/parcelas).toFixed(2)} reais.`)
 }
-else {
-    alert(`Você não digitou um valor numérico para as parcelas.`)
 }
-}
-else {alert(`Você não digitou nenhuma das opções.`)};
